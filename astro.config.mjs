22aeeb21e43bdd26d438fd1b86d8ce188d18e6c3
Christian Kozalla <christian.kozalla@gmail.com>
2022-12-27 22:16:53 +0100

    feat: set base url to /blog because of gh pages hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.christiankozalla.com",
+  base: "/blog",
   // output: "server",
   // adapter: node({
   //   mode: "standalone",
