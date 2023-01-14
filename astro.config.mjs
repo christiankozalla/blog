53593144a22c9c7576cae05d18ec8bbf5b3b98ad
Christian Kozalla <christian.kozalla@gmail.com>
2023-01-14 17:16:00 +0100

    fix: remove /blog url prefix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.christiankozalla.com",
-  base: "/blog/",
   outDir: "./dist",
   // output: "server",
   // adapter: node({
