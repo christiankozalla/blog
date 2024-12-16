4b69f8a18730aa97dc982fc04dc1bacbf14288b8
Christian Kozalla <christian.kozalla@gmail.com>
2024-12-16 21:59:34 +0100

    read only files

--- src/lib/articles.ts
+++ src/lib/articles.ts
@@ -28,37 +28,40 @@ const postsDirectory = path.join(process.cwd(), "articles");
 
 export function getSortedPostsData() {
   // Get file names under /posts
-  const fileNames = fs.readdirSync(postsDirectory);
-  const allPostsData = fileNames.map((fileName) => {
-    // Remove ".md" from file name to get id
-    const id = fileName.replace(/\.md$/, "");
+  const fileNames = fs.readdirSync(postsDirectory, { withFileTypes: true });
+  const allPostsData = fileNames
+    .filter((dirent) => dirent.isFile())
+    .map((dirent) => dirent.name)
+    .map((fileName) => {
+      // Remove ".md" from file name to get id
+      const id = fileName.replace(/\.md$/, "");
 
-    // Read markdown file as string
-    const fullPath = path.join(postsDirectory, fileName);
-    const fileContents = fs.readFileSync(fullPath, "utf8");
+      // Read markdown file as string
+      const fullPath = path.join(postsDirectory, fileName);
+      const fileContents = fs.readFileSync(fullPath, "utf8");
 
-    // Use gray-matter to parse the post metadata section
-    const matterResult = matter(fileContents);
+      // Use gray-matter to parse the post metadata section
+      const matterResult = matter(fileContents);
 
-    // Add readingTime to matterResult.data to access it outside /pages/posts/[id].js
-    const words = matterResult.content.split(" ").length;
-    const wordsPerMinute = 130;
-    const readingTime = Math.floor(words / wordsPerMinute).toString();
+      // Add readingTime to matterResult.data to access it outside /pages/posts/[id].js
+      const words = matterResult.content.split(" ").length;
+      const wordsPerMinute = 130;
+      const readingTime = Math.floor(words / wordsPerMinute).toString();
 
-    matterResult.data.readingTime = readingTime;
+      matterResult.data.readingTime = readingTime;
 
-    const formattedDate = format(
-      parseISO(matterResult.data.date),
-      "do 'of' MMM ''yy",
-    );
-    matterResult.data.formattedDate = formattedDate;
+      const formattedDate = format(
+        parseISO(matterResult.data.date),
+        "do 'of' MMM ''yy",
+      );
+      matterResult.data.formattedDate = formattedDate;
 
-    // Combine the data with the id
-    return {
-      id,
-      ...matterResult.data,
-    } as ArticleFrontmatter;
-  });
+      // Combine the data with the id
+      return {
+        id,
+        ...matterResult.data,
+      } as ArticleFrontmatter;
+    });
   // Sort posts by date
   return allPostsData.sort((a, b) => a.date < b.date ? 1 : -1);
 }
